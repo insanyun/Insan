d0ddd07fac3efa70de527da7e576fe566f8dfbe9
insan Yoon <insanyun@naver.com>
2017-08-31 16:56:33 +0900

    draft.

--- Hello_World/Hello_World/소스.cpp
+++ Hello_World/Hello_World/소스.cpp
@@ -1,7 +1,27 @@
 #include<iostream>
+#include<fstream>
+#include<sstream>
+#include <chrono>
 
 void main()
 {
-	std::cout << "Hello " << std::endl;
-	
+	using namespace std;
+
+    ifstream inputfile("sentence.txt");
+	string input_string;
+
+	stringstream string_stream;
+	string_stream << inputfile.rdbuf();//read the file
+	input_string = string_stream.str();//str holds the content of the file
+
+	cout << input_string << endl;//you can do anything with the string!!!
+
+	string typed_string;
+	auto begin = std::chrono::high_resolution_clock::now();
+	std::getline(std::cin, typed_string);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << std::chrono::duration_cast < std::chrono::milliseconds > (end - begin).count() /1000.0 << "s" << std::endl;
+	std::cout << "Compare" << typed_string.compare(input_string) << std::endl;
+
+
 }
