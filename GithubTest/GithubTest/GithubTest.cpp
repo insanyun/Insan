547987d69f06dd8cf4e184f8d8f564cf1b7cff8c
insan Yoon <insanyun@naver.com>
2017-08-31 14:42:04 +0900

    수정

---
+++ GithubTest/GithubTest/GithubTest.cpp
@@ -0,0 +1,14 @@
+// GithubTest.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+    return 0;
+}
+
---
+++ GithubTest/GithubTest/소스.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "stdafx.h"
+void main()
+{
+	std::cout << "Hello" << std::endl;
+	//return 0;
+}
+ 
